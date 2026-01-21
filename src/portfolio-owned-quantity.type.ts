7e60ad04690e89b51a9bdcd32004b242c0ebba3b
Sergio Pola <sergio@4usoft.cl>
2026-01-21 00:25:27 -0300

    refactor: use TS sintaxis

--- src/portfolio-owned-quantity.type.ts
+++ src/portfolio-owned-quantity.type.ts
@@ -1,8 +1,9 @@
 import { Stock } from "./stock";
 
-export type PortfolioOwnedQuantity = {
-    [key: string]: {
-        quantity: number;
-        stock: Stock;
-    }
-}
+/**
+ * Tipo que representa las acciones asignadas a un portfolio.
+ * @property {string} key - El ticker de la acción.
+ * @property {number} quantity - La cantidad de la acción.
+ * @property {Stock} stock - La acción.
+ */
+export type PortfolioOwnedQuantity = Record<string, { quantity: number; stock: Stock }>;
